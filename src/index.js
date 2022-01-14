efe49674867c01fea1c5bd4d80cc692dfada154b
SlavikSumish <ssshevchenko1983@gmail.com>
2022-01-14 13:43:19 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import './css/styles.css';
 
-// import debounce from 'lodash.debounce'
-// import Notiflix from 'notiflix'
+import debounce from 'lodash.debounce'
+import Notiflix from 'notiflix'
 import { fetchCountries } from './js/fetch-countries'
 
 const DEBOUNCE_DELAY = 300;
 
-// const countryInput = document.querySelector('#search-box')
-// const countryList = document.querySelector('.country-list')
-// const countryInfo = document.querySelector('.country-info')
+const countryInput = document.querySelector('#search-box')
+const countryList = document.querySelector('.country-list')
+const countryInfo = document.querySelector('.country-info')
 
-// countryInput.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY))
+countryInput.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY))
 
-// function onCountryInput() {
-//   const name = countryInput.value.trim()
-//   if (name === '') {
-//     return (countryList.innerHTML = ''), (countryInfo.innerHTML = '')
-//   }
+function onCountryInput() {
+  const name = countryInput.value.trim()
+  if (name === '') {
+    return (countryList.innerHTML = ''), (countryInfo.innerHTML = '')
+  }
 
   fetchCountries(name)
     .then(countries => {
@@ -34,39 +34,39 @@ const DEBOUNCE_DELAY = 300;
     .catch(alertWrongName)
 }
 
-// function renderCountryList(countries) {
-//   const markup = countries
-//     .map(({ name, flags }) => {
-//       return `
-//           <li class="country-list__item">
-//               <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
-//               <h2 class="country-list__name">${name.official}</h2>
-//           </li>
-//           `
-//     })
-//     .join('')
-//   return markup
-// }
+function renderCountryList(countries) {
+  const markup = countries
+    .map(({ name, flags }) => {
+      return `
+          <li class="country-list__item">
+              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+              <h2 class="country-list__name">${name.official}</h2>
+          </li>
+          `
+    })
+    .join('')
+  return markup
+}
 
-// function renderCountryInfo(countries) {
-//   const markup = countries
-//     .map(({ capital, population, languages }) => {
-//       return `
-//         <ul class="country-info__list">
-//             <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
-//             <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
-//             <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
-//         </ul>
-//         `
-//     })
-//     .join('')
-//   return markup
-// }
+function renderCountryInfo(countries) {
+  const markup = countries
+    .map(({ capital, population, languages }) => {
+      return `
+        <ul class="country-info__list">
+            <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
+            <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
+            <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
+        </ul>
+        `
+    })
+    .join('')
+  return markup
+}
 
-// function alertWrongName() {
-//   Notiflix.Notify.failure('Oops, there is no country with that name')
-// }
+function alertWrongName() {
+  Notiflix.Notify.failure('Oops, there is no country with that name')
+}
 
-// function alertTooManyMatches() {
-//   Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
-// }
+function alertTooManyMatches() {
+  Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+}
